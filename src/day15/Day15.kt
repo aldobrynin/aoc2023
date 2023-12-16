9ee4a657663bb46d8f28fd020426b9957b6544ea
Alexander Dobrynin <dobrynin.lpv@gmail.com>
2023-12-16 12:57:03 +0400

    use built-in LinkedHashMaps to handle operations

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -4,7 +4,6 @@ import common.Puzzle
 import solveAndVerify
 
 private typealias ParsedInput = List<String>
-private typealias Lens = Pair<String, Int>
 
 private class Day15 : Puzzle<ParsedInput>(day = 15) {
     override fun parse(rawInput: List<String>): ParsedInput = rawInput.single().split(',')
@@ -12,21 +11,15 @@ private class Day15 : Puzzle<ParsedInput>(day = 15) {
     override fun part1(input: ParsedInput): Long = input.sumOf { it.hash() }.toLong()
 
     override fun part2(input: ParsedInput): Long {
-        val boxes = Array<MutableList<Lens>>(256) { mutableListOf() }
+        val boxes = mutableMapOf<Int, MutableMap<String, Int>>()
         for (instruction in input.map { it.split('=', '-') }) {
             val (label, lens) = instruction
-            val box = boxes[label.hash()]
-            val index = box.indexOfFirst { it.first == label }
-            if (lens.isEmpty()) {
-                if (index != -1) box.removeAt(index)
-            } else {
-                if (index != -1) box[index] = label to lens.toInt()
-                else box.add(label to lens.toInt())
-            }
+            val box = boxes.getOrPut(label.hash()) { mutableMapOf() }
+            if (lens.isEmpty()) box.remove(label)
+            else box[label] = lens.toInt()
         }
-
-        return boxes
-            .flatMapIndexed { boxId, box -> box.mapIndexed { slot, lens -> (boxId + 1L) * (slot + 1) * lens.second } }
+        return boxes.entries
+            .flatMap { box -> box.value.entries.mapIndexed { slot, lens -> (box.key + 1L) * (slot + 1) * lens.value } }
             .sum()
     }
 
